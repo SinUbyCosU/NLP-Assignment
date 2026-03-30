288eee990cebf2c80e41bc8cb6b16c7f243b7daa
SinUbyCosU <yadav23@iiserb.ac.in>
2026-03-31 01:35:04 +0530

    LSTM

--- Exam code/exam_Code.py
+++ Exam code/exam_Code.py
@@ -37,3 +37,13 @@ model_rnn = Sequential([
 model_rnn.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 model_rnn.fit(X, y, epochs=100, verbose=0)
 
+#LSTM
+
+model_lstm = Sequential([
+    Embedding(vocab_size, 10, input_length=max_len-1),
+    LSTM(50),
+    Dense(vocab_size, activation='softmax')
+])
+model_lstm.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model_lstm.fit(X, y, epochs=100, verbose=0)
+
